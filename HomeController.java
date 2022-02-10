84011e6ab0908d58f11ed952cabba425bfb553f7
Jiten <jiten.ahuja47@gmail.com>
2022-02-10 12:01:46 +0530

    first commit

---
+++ HomeController.java
@@ -0,0 +1,26 @@
+package com.telusko.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("home")
+	public ModelAndView home(Alien alien) //home name u can change it is not mandatory that it will be home only.
+	{
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("obj", alien);
+		mv.setViewName("home");
+		
+		return mv;
+	}
+	
+
+}
